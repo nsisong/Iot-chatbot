2ae29f9f360e98d94433a924c5b0afa04172bdac
nsisong <edetnsisongpeter@gmail.com>
2022-03-16 18:07:22 +0100

    create folder for the peech to text

--- Speech to text/Speech to text.py
+++ Speech to text/Speech to text.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
+    print("ok you can speek now")
     audio_data = r.record(source, duration= 10)
     print("Recognizing .....")
     text = r.recognize_google(audio_data)
