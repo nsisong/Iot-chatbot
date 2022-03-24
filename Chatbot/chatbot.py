489b12c717021f0405ed3ad9195124a3d855c29c
nsisong <edetnsisongpeter@gmail.com>
2022-03-24 14:08:55 +0100

    set two voices, male and femal

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 import time
 import speech_recognition as sr
 import pyttsx3
@@ -12,15 +12,25 @@ engine = pyttsx3.init()
 led_alarm = 18
 buzzer_alarm = 31
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(led_alarm, GPIO.OUT)
-GPIO.setup(buzzer_alarm, GPIO.OUT)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(led_alarm, GPIO.OUT)
+# GPIO.setup(buzzer_alarm, GPIO.OUT)
 
 with mic as source:
     r.adjust_for_ambient_noise(source)
+    
+    #set two voices, indext 0 for male and 1 for female
+    voices = engine.getProperty("voices")
+    print("male voice: {0}". format(voices[0].id))
+    print("female voice: {0}". format(voices[1].id))
+
+    #set voice to be used
+    engine.setProperty("voice", voices[0].id)
+
     print("ok you can speek now")
     # audio_data = r.record(source, duration= 5)
     audio_data = r.listen(source)
+
     
     try:
         print("Recognizing .....")
@@ -30,7 +40,13 @@ with mic as source:
         engine.runAndWait()
         
     except sr.UnknownValueError:
-        print("don't know what you said")
+        err = "don't know what you said"
+        engine.say(err)
+        engine.runAndWait()
+        print(err)
 
     except sr.RequestError as e:
-        print ("unable to request result from Google Speech Recognition Server:{}".format(e))
+        requesterror = "unable to request result from Google Speech Recognition Server:{}".format(e)
+        print (requesterror)
+        engine.say(requesterror)
+        engine.runAndWait()
