6c186f5865ca380ab93a76161e86a9c338b43457
Nsisong <nsisong@Nsisongs-MacBook-Air.local>
2022-03-31 14:07:25 +0100

    fix error with mic

--- Chatbot/chatbot.py
+++ Chatbot/chatbot.py
@@ -16,37 +16,37 @@ buzzer_alarm = 31
 # GPIO.setup(led_alarm, GPIO.OUT)
 # GPIO.setup(buzzer_alarm, GPIO.OUT)
 
-with mic as source:
-    r.adjust_for_ambient_noise(source)
-    
-    #set two voices, indext 0 for male and 1 for female
-    voices = engine.getProperty("voices")
-    print("male voice: {0}". format(voices[0].id))
-    print("female voice: {0}". format(voices[1].id))
+#set two voices, indext 0 for male and 1 for female
+voices = engine.getProperty("voices")
+print("male voice: {0}". format(voices[0].id))
+print("female voice: {0}". format(voices[1].id))
 
-    #set voice to be used
-    engine.setProperty("voice", voices[0].id)
-
-    print("ok you can speek now")
-    # audio_data = r.record(source, duration= 5)
-    audio_data = r.listen(source)
-
-    
-    try:
-        print("Recognizing .....")
-        text = r.recognize_google(audio_data)
-        print(text)
-        engine.say(text)
-        engine.runAndWait()
+while True:
+    with mic as source:
+        r.adjust_for_ambient_noise(source, duration=0.2)
         
-    except sr.UnknownValueError:
-        err = "don't know what you said"
-        engine.say(err)
-        engine.runAndWait()
-        print(err)
+        #set voice to be used
+        engine.setProperty("voice", voices[0].id)
 
-    except sr.RequestError as e:
-        requesterror = "unable to request result from Google Speech Recognition Server:{}".format(e)
-        print (requesterror)
-        engine.say(requesterror)
-        engine.runAndWait()
+        print("ok you can speek now")
+        audio_data = r.listen(source)
+
+        
+        try:
+            print("Recognizing .....")
+            text = r.recognize_google(audio_data)
+            print(text)
+            engine.say(text)
+            engine.runAndWait()
+            
+        except sr.UnknownValueError:
+            err = "don't know what you said"
+            engine.say(err)
+            engine.runAndWait()
+            print(err)
+
+        except sr.RequestError as e:
+            requesterror = "unable to request result from Google Speech Recognition Server:{}".format(e)
+            print (requesterror)
+            engine.say(requesterror)
+            engine.runAndWait()
--- Speech to text/Speech to text.py
+++ Speech to text/Speech to text.py
@@ -3,7 +3,8 @@ r = sr.Recognizer()
 
 with sr.Microphone() as source:
     print("ok you can speek now")
-    audio_data = r.record(source, duration= 5)
+    # audio_data = r.record(source, duration= 5)
+    audio = r.listen(source)
     print("Recognizing .....")
-    text = r.recognize_google(audio_data)
+    text = r.recognize_google(audio)
     print(text)
